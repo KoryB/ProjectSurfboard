ae8e0fb00b719836b4207e0d6a3f0ad6a7331875
Michael Wollard <michaelawollard@gmail.com>
2016-01-16 00:29:41 -0500

    Created Game class somewhat filled in. Will merge with Screen branch and continue incorporation there.

---
+++ src/framework/Game.java
@@ -0,0 +1,25 @@
+package framework;
+
+/**
+ * Created by Michael on 1/15/2016.
+ */
+public class Game {
+
+    Screen activeScreen;
+
+    public Game(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void render(){
+
+    }
+
+    public void run(){
+
+    }
+}
--- src/framework/ScreenInterface.java
+++
@@ -1,7 +0,0 @@
-package framework;
-
-/**
- * Created by Michael on 1/15/2016.
- */
-public class ScreenInterface {
-}
