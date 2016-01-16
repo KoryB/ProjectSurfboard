e5dfa61cdc7c975172a1bb0448c15f6300bf673a
Michael Wollard <michaelawollard@gmail.com>
2016-01-16 00:36:29 -0500

    Ayy

--- src/framework/Game.java
+++ src/framework/Game.java
@@ -5,21 +5,27 @@ package framework;
  */
 public class Game {
 
-    Screen activeScreen;
+    Screen mActiveScreen;
 
     public Game(){
-
+        //Starting with a game screen for now until we have the main menu implemented
+        mActiveScreen = new GameScreen();
     }
 
     public void update(){
-
+        mActiveScreen.update();
     }
 
     public void render(){
-
+        mActiveScreen.render();
     }
 
     public void run(){
+        update();
+        render();
+    }
 
+    public void setActiveScreen(Screen screen){
+        mActiveScreen = screen;
     }
 }
