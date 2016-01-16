19cd8481d07ded8096284eadf2c229a63647d585
Michael Wollard <michaelawollard@gmail.com>
2016-01-15 23:34:16 -0500

    Added a few instance variables in MainMenuScreen.java that I forgot initially.

--- src/framework/MainMenuScreen.java
+++ src/framework/MainMenuScreen.java
@@ -3,5 +3,37 @@ package framework;
 /**
  * Created by Michael on 1/15/2016.
  */
-public class MainMenuScreen {
+public class MainMenuScreen implements ScreenInterface{
+
+    //InputHandler mInput;
+    boolean mPaused;
+
+    public MainMenuScreen(){
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
 }
