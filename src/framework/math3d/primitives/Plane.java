d541d9913522597278e53bf9f39eb3edc55edc2f
koryb <byrnek@mymail.shawnee.edu>
2016-01-21 11:08:12 -0500

    Fixed michael's silly problem

--- src/framework/math3d/primitives/Plane.java
+++ src/framework/math3d/primitives/Plane.java
@@ -1,8 +1,21 @@
 package framework.math3d.primitives;
 
+import framework.math3d.vec4;
+
 /**
  * Created by kory on 1/21/16.
  */
-public class Plane
+public class Plane implements Primitive
 {
+    @Override
+    public void translate(vec4 amount)
+    {
+
+    }
+
+    @Override
+    public void printInfo()
+    {
+
+    }
 }
