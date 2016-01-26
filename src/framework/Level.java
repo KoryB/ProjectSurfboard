5f599f2c23f6d2bc98667a419894f7a73ae3d522
Michael Wollard <michaelawollard@gmail.com>
2016-01-26 10:46:28 -0500

    Added skeleton for room, hallway, and level.

---
+++ src/framework/Level.java
@@ -0,0 +1,28 @@
+package framework;
+
+import framework.math3d.*;
+
+/**
+ * Created by Michael on 1/26/2016.
+ */
+public class Level {
+
+    public Room[] mRooms;
+    public Hallway[] mHallways;
+
+    public Level(){
+
+    }
+
+    public void generateLevel(){
+
+    }
+
+    public void generateRoom(){
+
+    }
+
+    public void generateHallway(){
+
+    }
+}
---
+++ src/framework/Room.java
@@ -0,0 +1,16 @@
+package framework;
+
+import framework.math3d.*;
+
+/**
+ * Created by Michael on 1/26/2016.
+ */
+public class Room {
+
+    public Wall[] mWalls;
+    public Floor mFloor;
+
+    public Room(){
+
+    }
+}
