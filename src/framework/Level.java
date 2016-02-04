1d4b4cc251723aa043ea07090eb26a58b1698456
Michael Wollard <michaelawollard@gmail.com>
2016-02-04 09:09:56 -0500

    Level generation and rendering is working, just need updated walls and floors so it looks correct, later will add enemy creation and loot creation to the level gen.

--- src/framework/Level.java
+++ src/framework/Level.java
@@ -28,16 +28,17 @@ public class Level {
         mTiles = new CollisionObject[(int) dimensions.x][(int) dimensions.y];
         mStartingCorner = new vec4((int) mXRange.x, 0, (int) mZRange.x, 1);
 
-        genEmptyLevel();
-        genFloors();
+        genNewLevel();
         System.out.println();
 
         for(int i = 0; i < mTiles.length; i++){
             for(int j = 0; j < mTiles[0].length; j++){
                 if(mTiles[i][j] instanceof Floor)
-                    System.out.print("_");
+                    System.out.print("_ ");
+                else if(mTiles[i][j] instanceof Wall)
+                    System.out.print("# ");
                 else
-                    System.out.print("#");
+                    System.out.print("  ");
             }
             System.out.println();
         }
@@ -56,17 +57,24 @@ public class Level {
     private void genFloors(){
         //Find Starting Tile & place floor
         Random rand = new Random();
-        int currentX = Math.max(rand.nextInt((int) mDimensions.x - 1), 1);
-        int currentZ = Math.max(rand.nextInt((int) mDimensions.y - 1), 1);
+        int currentX = (int) (mDimensions.x / 2);
+        int currentZ = (int) (mDimensions.y / 2);
 
         vec4 pos = new vec4(currentX, 0, currentZ, 0);
         mTiles[currentX][currentZ] = new Floor(pos.add(mStartingCorner));
 
         int numFloors = 1;
+        int direction;
+        int oppositeDirection = 100;
 
-        //Do random walk algorithm until corrent percent of tiles are floors
+        //Do random walk algorithm until correct percent of tiles are floors
         while (numFloors < (int) (mNumTiles * mPercentFloor)){
-            int direction = rand.nextInt(4);
+            direction = rand.nextInt(4);
+
+            //Isn't allowed to walk directly backwards.
+            while (direction == oppositeDirection){
+                direction = rand.nextInt(4);
+            }
 
             //Walk in random direction and make floor as long as not walking onto edge tile
             //North
@@ -75,6 +83,7 @@ public class Level {
                     currentZ -= 1;
                     pos.x = currentX;
                     pos.z = currentZ;
+                    oppositeDirection = 2;
                     if(!(mTiles[currentX][currentZ] instanceof Floor)){
                         mTiles[currentX][currentZ] = new Floor(pos.add(mStartingCorner));
                         numFloors++;
@@ -87,6 +96,7 @@ public class Level {
                     currentX += 1;
                     pos.x = currentX;
                     pos.z = currentZ;
+                    oppositeDirection = 3;
                     if(!(mTiles[currentX][currentZ] instanceof Floor)){
                         mTiles[currentX][currentZ] = new Floor(pos.add(mStartingCorner));
                         numFloors++;
@@ -99,6 +109,7 @@ public class Level {
                     currentZ += 1;
                     pos.x = currentX;
                     pos.z = currentZ;
+                    oppositeDirection = 0;
                     if(!(mTiles[currentX][currentZ] instanceof Floor)){
                         mTiles[currentX][currentZ] = new Floor(pos.add(mStartingCorner));
                         numFloors++;
@@ -111,6 +122,7 @@ public class Level {
                     currentX -= 1;
                     pos.x = currentX;
                     pos.z = currentZ;
+                    oppositeDirection = 1;
                     if(!(mTiles[currentX][currentZ] instanceof Floor)){
                         mTiles[currentX][currentZ] = new Floor(pos.add(mStartingCorner));
                         numFloors++;
@@ -120,7 +132,90 @@ public class Level {
         }
     }
 
-    public void genNewLevel(){
+    private void clearOutsideWalls(){
+        for(int i = 0; i < mTiles.length; i++){
+            boolean outside = false;
+            for(int j = 0; j < mTiles[0].length; j++){
+                if(mTiles[i][j] instanceof Wall){
 
+                    if(i == 0){
+                        if(j == 0){
+                            if(!(mTiles[i+1][j] instanceof Floor) && !(mTiles[i][j+1] instanceof Floor) && !(mTiles[i+1][j+1] instanceof Floor)){
+                                mTiles[i][j] = null;
+                            }
+                        }
+                        else if(j == mDimensions.y - 1){
+                            if(!(mTiles[i+1][j] instanceof Floor) && !(mTiles[i][j-1] instanceof Floor) && !(mTiles[i+1][j-1] instanceof Floor)){
+                                mTiles[i][j] = null;
+                            }
+
+                        }
+                        else{
+                            if(!(mTiles[i+1][j] instanceof Floor) && !(mTiles[i][j-1] instanceof Floor) && !(mTiles[i][j+1] instanceof Floor) && !(mTiles[i+1][j-1] instanceof Floor) && !(mTiles[i+1][j+1] instanceof Floor)){
+                                mTiles[i][j] = null;
+                            }
+                        }
+
+                    }
+
+                    else if(j == 0){
+                        if(i == mDimensions.x - 1){
+                            if(!(mTiles[i][j+1] instanceof Floor) && !(mTiles[i-1][j] instanceof Floor) && !(mTiles[i-1][j+1] instanceof Floor)){
+                                mTiles[i][j] = null;
+                            }
+                        }
+                        else{
+                            if(!(mTiles[i+1][j] instanceof Floor) && !(mTiles[i-1][j] instanceof Floor) && !(mTiles[i][j+1] instanceof Floor) && !(mTiles[i+1][j+1] instanceof Floor) && !(mTiles[i-1][j+1] instanceof Floor)){
+                                mTiles[i][j] = null;
+                            }
+                        }
+                    }
+
+                    else if(i == mDimensions.x - 1){
+                        if(j == mDimensions.y - 1){
+                            if(!(mTiles[i][j-1] instanceof Floor) && !(mTiles[i-1][j] instanceof Floor) && !(mTiles[i-1][j-1] instanceof Floor)){
+                                mTiles[i][j] = null;
+                            }
+                        }
+                        else{
+                            if(!(mTiles[i][j+1] instanceof Floor) && !(mTiles[i][j-1] instanceof Floor) && !(mTiles[i-1][j] instanceof Floor) && !(mTiles[i-1][j+1] instanceof Floor) && !(mTiles[i-1][j-1] instanceof Floor)){
+                                mTiles[i][j] = null;
+                            }
+                        }
+                    }
+
+                    else if(j == mDimensions.y - 1){
+                        if(!(mTiles[i][j-1] instanceof Floor) && !(mTiles[i+1][j] instanceof Floor) && !(mTiles[i-1][j] instanceof Floor) && !(mTiles[i+1][j-1] instanceof Floor) && !(mTiles[i-1][j-1] instanceof Floor)){
+                            mTiles[i][j] = null;
+                        }
+                    }
+
+                    else{
+                        if(!(mTiles[i][j+1] instanceof Floor) && !(mTiles[i][j-1] instanceof Floor) && !(mTiles[i+1][j] instanceof Floor) && !(mTiles[i-1][j] instanceof Floor) && !(mTiles[i-1][j-1] instanceof Floor) && !(mTiles[i-1][j+1] instanceof Floor) && !(mTiles[i+1][j-1] instanceof Floor) && !(mTiles[i+1][j+1] instanceof Floor)){
+                            mTiles[i][j] = null;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void genNewLevel(){
+        genEmptyLevel();
+        genFloors();
+        clearOutsideWalls();
+    }
+
+    public void draw(Program program){
+        for(int i = 0; i < mTiles.length; i++){
+            for(int j = 0; j < mTiles[0].length; j++){
+                if(mTiles[i][j] instanceof Wall){
+                    ((Wall) mTiles[i][j]).draw(program);
+                }
+                else if(mTiles[i][j] instanceof Floor){
+                    ((Floor) mTiles[i][j]).draw(program);
+                }
+            }
+        }
     }
 }
